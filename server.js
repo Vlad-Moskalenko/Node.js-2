01565e03d2190d015bb5837867465f70ca062db8
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-05-11 08:59:28 +0300

    fix password

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
-
-const DB_HOST = `mongodb+srv://vlad:${process.env.PASSWORD}@cluster0.jznjmq3.mongodb.net/contacts_reader?retryWrites=true&w=majority`;
+const DB_HOST =
+  'mongodb+srv://vlad:vlad414851@cluster0.jznjmq3.mongodb.net/contacts_reader?retryWrites=true&w=majority';
 
 mongoose
   .connect(DB_HOST)
