284a6a110e16d151e545b9c25a8bf64cbac35895
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-05-11 10:12:13 +0300

    init env variables

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const DB_HOST =
-  'mongodb+srv://vlad:vlad414851@cluster0.jznjmq3.mongodb.net/contacts_reader?retryWrites=true&w=majority';
-
-mongoose.set('strictQuery', true);
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
