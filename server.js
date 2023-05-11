2b024bbb34ac232d962c2a43faaaa092e222f4a8
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-05-11 08:43:41 +0300

    init mongoDB and mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require('dotenv').config();
+
+const DB_HOST = `mongodb+srv://vlad:${process.env.PASSWORD}@cluster0.jznjmq3.mongodb.net/contacts_reader?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1);
+  });
