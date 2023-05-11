897920c2a290d43819e0dd8778cc9c18f5ca0327
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-05-11 09:18:49 +0300

    fix deploy

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const DB_HOST =
   'mongodb+srv://vlad:vlad414851@cluster0.jznjmq3.mongodb.net/contacts_reader?retryWrites=true&w=majority';
 
+mongoose.set('strictQuery', true);
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log('Server running. Use our API on port: 3000');
-    });
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err.message);
