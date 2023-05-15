fef644d6cf349bc52dfb548fb4d340717fd58c6b
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-05-15 11:37:49 +0300

    init register and login

---
+++ routes/api/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const { validateBody } = require('../../middlewares');
+const { schemas } = require('../../models/user');
+const ctrl = require('../../controllers/auth');
+
+router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
+
+router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
+
+module.exports = router;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,21 +3,28 @@ const router = express.Router();
 const controllers = require('../../controllers/contacts');
 
 const { validateBody } = require('../../middlewares');
-const { isValidId } = require('../../middlewares');
+const { isValidId, authenticate } = require('../../middlewares');
 const { schemas } = require('../../models/contact');
 
-router.get('/', controllers.getContactsList);
+router.get('/', authenticate, controllers.getContactsList);
 
-router.get('/:id', isValidId, controllers.getContactById);
+router.get('/:id', authenticate, isValidId, controllers.getContactById);
 
-router.post('/', validateBody(schemas.schema), controllers.addContact);
+router.post('/', authenticate, validateBody(schemas.schema), controllers.addContact);
 
-router.delete('/:id', isValidId, controllers.removeContact);
+router.delete('/:id', authenticate, isValidId, controllers.removeContact);
 
-router.put('/:id', isValidId, validateBody(schemas.schema), controllers.updateContact);
+router.put(
+  '/:id',
+  authenticate,
+  isValidId,
+  validateBody(schemas.schema),
+  controllers.updateContact
+);
 
 router.patch(
   '/:id/favorite',
+  authenticate,
   validateBody(schemas.updateFavoriteSchema),
   controllers.updateFavorite
 );
